3b3ed0f3adf4712d95833a5cacefb5f2aedb26ea
Vlad Zaharia <hey@vlad.gg>
2023-07-21 21:08:39 -0700

    Player/GM-JWT tests

--- assassin-server/src/auth/gm.spec.ts
+++ assassin-server/src/auth/gm.spec.ts
@@ -3,6 +3,7 @@ import { GMAuth } from './gm'
 import { vi } from 'vitest'
 import { Context } from 'hono'
 import { AuthException } from './common'
+import { SignJWT } from 'jose'
 
 const mocks = vi.hoisted(() => {
 	return {
@@ -76,5 +77,27 @@ describe('GMAuth', () => {
 		mocks.findRoomMock.mockImplementationOnce(() => undefined)
 
 		expect(() => GMAuth(context)).rejects.toEqual(new AuthException('Room not found!', 404))
+	})
+
+	test('auth uses JWT if normal user header is not defined', async () => {
+		const token = await new SignJWT({ assassin: { admin: false, user: true }, first_name: "test-player" })
+			.setProtectedHeader({ alg: 'HS256' })
+			.sign(new TextEncoder().encode('some-test-secret'))
+
+		context.req.header = (key) => key === "Authorization" ? `Bearer ${token}` : undefined
+
+		const result = await GMAuth(context)
+		expect(result).toBeTruthy()
+	})
+
+	test('auth is unsuccessful if on a different player, using JWT', async () => {
+		const token = await new SignJWT({ assassin: { admin: false, user: true }, first_name: "Vlad" })
+			.setProtectedHeader({ alg: 'HS256' })
+			.sign(new TextEncoder().encode('some-test-secret'))
+
+		context.req.header = (key) => key === "Authorization" ? `Bearer ${token}` : undefined
+
+		const result = await GMAuth(context)
+		expect(result).toBeFalsy()
 	})
 })
--- assassin-server/src/auth/player.spec.ts
+++ assassin-server/src/auth/player.spec.ts
@@ -1,5 +1,6 @@
 import { Context } from 'hono'
 import { PlayerAuth } from './player'
+import { SignJWT } from 'jose'
 
 let context: Context<{ Bindings }>
 
@@ -28,6 +29,28 @@ describe('PlayerAuth', () => {
 
 	test('auth is unsuccessful if on a different player', async () => {
 		context.req.header = () => 'Vlad'
+
+		const result = await PlayerAuth(context)
+		expect(result).toBeFalsy()
+	})
+
+	test('auth uses JWT if normal user header is not defined', async () => {
+		const token = await new SignJWT({ assassin: { admin: false, user: true }, first_name: "test-player" })
+			.setProtectedHeader({ alg: 'HS256' })
+			.sign(new TextEncoder().encode('some-test-secret'))
+
+		context.req.header = (key) => key === "Authorization" ? `Bearer ${token}` : undefined
+
+		const result = await PlayerAuth(context)
+		expect(result).toBeTruthy()
+	})
+
+	test('auth is unsuccessful if on a different player, using JWT', async () => {
+		const token = await new SignJWT({ assassin: { admin: false, user: true }, first_name: "Vlad" })
+			.setProtectedHeader({ alg: 'HS256' })
+			.sign(new TextEncoder().encode('some-test-secret'))
+
+		context.req.header = (key) => key === "Authorization" ? `Bearer ${token}` : undefined
 
 		const result = await PlayerAuth(context)
 		expect(result).toBeFalsy()
